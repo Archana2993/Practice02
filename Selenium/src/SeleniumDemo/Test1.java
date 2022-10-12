6b4e38ba55ae3b77cedf30d25636e7e1b90e2b49
Archana2993 <archanashejul2@gmail.com>
2022-10-12 15:42:46 +0530

    selenium code updated

--- Selenium/src/SeleniumDemo/Test1.java
+++ Selenium/src/SeleniumDemo/Test1.java
@@ -2,9 +2,11 @@ package SeleniumDemo;
 
 import java.io.File;
 import java.io.FileInputStream;
-
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellType;
@@ -12,25 +14,20 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Test1 {
 
 	public static void main(String[] args) throws  IOException
 	{
-		String data;
-		File path=new File("C:\\Users\\Samir\\Desktop\\Credential.xlsx");
-		FileInputStream file=new FileInputStream(path);
-		Workbook wb= WorkbookFactory.create(file);
-		Sheet sheetName=wb.getSheet("Login data");
-		Row rowNum=sheetName.getRow(2);
-		Cell cellNum=rowNum.getCell(0);
-		CellType type=cellNum.getCellType();
-		// System.out.println(cellNum.getCellType());
-	//	data=cellNum.getStringCellValue();
-		
-	    
+	 String path="";
+	 
 	  
-	
+	  
+	  
+	 
 		
 	}
 
